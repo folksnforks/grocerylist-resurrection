5d4b15365d8e8ac836096ced07e6eaadf709c14a
folksnforks <folksnforks13@gmail.com>
2022-01-06 00:17:31 -0800

    2) Finished delete button. Delete and edit buttons have been assigned to JS variables for eventListeners.

--- index.js
+++ index.js
@@ -9,17 +9,32 @@ function handleSubmit(event) {
    event.preventDefault();
    
    const itemList = document.createElement('p');
-
+   
    itemList.innerHTML = `${inputBox.value}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                        <button id="edit_${++listNumber}">Edit</button>
-                        <button id="delete_${listNumber}">Delete</button>`;
-
+   <button id="edit_${++listNumber}">Edit</button>
+   <button id="delete_${listNumber}">Delete</button>`;
+   
+   
    console.log('itemList.innerHTML', itemList.innerHTML);
    // 1) click in inputBox, and it consolelogs "empty string"
    // 2) then type text into inputBox
    // 3) click submit button, and browser appears to refresh/reset?
    listSection.appendChild(itemList);
+   
+   const deleteButton = document.getElementById('delete_' + listNumber);
+   const editButton = document.getElementById('edit_' + listNumber);
+
+   editButton.addEventListener('click', handleEdit);
+   deleteButton.addEventListener('click', handleDelete);
    inputBox.value = '';
+}
+
+function handleEdit () {
+
+}
+
+function handleDelete (event) {
+   event.target.parentElement.remove();
 }
 
 submitButton.addEventListener('click', handleSubmit);
