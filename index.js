40d51e33be4488858a6a2afcecbe5f9f7baf35aa
folksnforks <folksnforks13@gmail.com>
2022-01-09 16:13:51 -0800

    3) Edit button now populates textbox w/ name of selected item.

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ function handleSubmit(event) {
    event.preventDefault();
    
    const itemList = document.createElement('p');
-   
+
    itemList.innerHTML = `${inputBox.value}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
    <button id="edit_${++listNumber}">Edit</button>
-   <button id="delete_${listNumber}">Delete</button>`;
+   <button id="delete_${listNumber}">Delete</button>`;   
    
-   
-   console.log('itemList.innerHTML', itemList.innerHTML);
+   // console.log('itemList.innerHTML', itemList.innerHTML);
    // 1) click in inputBox, and it consolelogs "empty string"
    // 2) then type text into inputBox
    // 3) click submit button, and browser appears to refresh/reset?
@@ -29,11 +28,17 @@ function handleSubmit(event) {
    inputBox.value = '';
 }
 
-function handleEdit () {
+function handleEdit (event) {
+   inputBox.value = event.target.parentElement.innerHTML.split("&nbsp", 1);
+   submitButton.textContent = "Finish Editing";
 
+   // inputBox.value = ;
+
+   // submitButton.innerHTML = "Add to list";
 }
 
-function handleDelete (event) {
+function handleDelete 
+(event) {
    event.target.parentElement.remove();
 }
 
